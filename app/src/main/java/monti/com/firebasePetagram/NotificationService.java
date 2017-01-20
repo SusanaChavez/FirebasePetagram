881cd1bec71c06e335cde4521aef9cd09012b332
Susana <susana.chvz@gmail.com>
2017-01-19 22:11:15 -0300

    Android Wear

--- app/src/main/java/monti/com/firebasePetagram/NotificationService.java
+++ app/src/main/java/monti/com/firebasePetagram/NotificationService.java
@@ -46,16 +46,36 @@ public class NotificationService extends FirebaseMessagingService {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
         //Intent i = new Intent(this, MainActivity.class);
-        Intent i = new Intent();
-        i.setAction("VER_PERFIL");
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
 
         Uri sonido = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        NotificationCompat.Action action =
+        Intent i1 = new Intent();
+        i1.setAction("VER_PERFIL");
+        PendingIntent pendingIntent1 = PendingIntent.getBroadcast(this, 0, i1, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent i2 = new Intent();
+        i2.setAction("SEGUIR");
+        PendingIntent pendingIntent2 = PendingIntent.getBroadcast(this, 0, i2, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent i3 = new Intent();
+        i3.setAction("VER_USUARIO");
+        PendingIntent pendingIntent3 = PendingIntent.getBroadcast(this, 0, i3, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Action action1 =
                 new NotificationCompat.Action.Builder(R.drawable.ic_full_perfil,
-                        getString(R.string.texto_accion_perfil), pendingIntent)
-                .build();
+                        getString(R.string.texto_accion_perfil), pendingIntent1)
+                        .build();
+
+        NotificationCompat.Action action2 =
+                new NotificationCompat.Action.Builder(R.drawable.ic_full_perfil,
+                        getString(R.string.texto_seguir), pendingIntent2)
+                        .build();
+
+        NotificationCompat.Action action3 =
+                new NotificationCompat.Action.Builder(R.drawable.ic_full_usuario,
+                        getString(R.string.texto_seguir), pendingIntent2)
+                        .build();
+
 
         NotificationCompat.WearableExtender wearableExtender =
                 new NotificationCompat.WearableExtender()
@@ -68,8 +88,10 @@ public class NotificationService extends FirebaseMessagingService {
                 .setContentText(remoteMessage.getNotification().getBody())
                 .setAutoCancel(true)
                 .setSound(sonido)
-                .setContentIntent(pendingIntent)
-                .extend(wearableExtender.addAction(action))
+                .setContentIntent(pendingIntent1)
+                .extend(wearableExtender.addAction(action1))
+                .extend(wearableExtender.addAction(action2))
+                .extend(wearableExtender.addAction(action3))
                 //.addAction(R.drawable.ic_full_perfil, getString(R.string.texto_accion_perfil),pendingIntent)
 
                 ;
